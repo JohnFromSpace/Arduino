deed795716a3a14e8faf06abf1bc01ba9d502958
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-09 00:56:23 +0200

    Update Light Dependent Resistor.ino

--- Code/Light Dependent Resistor.ino
+++ Code/Light Dependent Resistor.ino
@@ -1,1 +1,12 @@
+int ledPin = 9; //pin for the LED
+int photoCellInput = 0; //pin for the photoresistor
 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  photoCellInput = (analogRead(0)/4); //conversion from 0-1023 to  0-255
+  analogWrite(ledPin, photoCellInput);
+  delay(20); //delaying it in order to achieve a dimming effect
+}
