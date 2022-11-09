3f824d6c3ae3d73781c90ecfb6f517d9391ecff5
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-11-09 15:36:55 +0200

    Update Blinking LED.ino

--- Code/Blinking LED.ino
+++ Code/Blinking LED.ino
@@ -1,9 +1,12 @@
-int ledPin = 13;
+int ledPin = 13; // declare the pin that the LED is connected to
 
 void setup() {
-
+  pinMode(ledPin, OUTPUT); // declare the pin as used for output
 }
 
 void loop() {
-  
-}
+  digitalWrite(ledPin, HIGH); // turns on the LED
+  delay(1000); // waits for 1 second
+  digitalWrite(ledPin, LOW); // turns off the LED
+  delay(1000); // waits for 1 second
+} // the cycle repeats indefinitely and thus we achieve the blinking effect
