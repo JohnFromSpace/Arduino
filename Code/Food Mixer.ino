1de1070ef82099702aa6ef312cab8ad625fa24eb
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-09 16:47:37 +0200

    Update Food Mixer.ino

--- Code/Food Mixer.ino
+++ Code/Food Mixer.ino
@@ -2,3 +2,7 @@
 const int motorPin = 9;          // Pin connected to the motor
 const int directionPin = 10;     // Pin connected to direction control
 const int potentiometerPin = A0; // Pin connected to potentiometer
+
+// Define motor directions
+const int clockwise = HIGH;
+const int counterclockwise = LOW;
