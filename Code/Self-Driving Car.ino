766735ad19c9867c791026d1267853f512f3aa47
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-11 22:53:21 +0200

    Update Self-Driving Car.ino

--- Code/Self-Driving Car.ino
+++ Code/Self-Driving Car.ino
@@ -105,5 +105,9 @@ void loop() {
 void turnLeft() {
     int leftSpeed = constrain(BASE_SPEED - output, -255, 255);
     int rightSpeed = constrain(BASE_SPEED + output, -255, 255);
-     
+
+    analogWrite(LEFT_MOTOR_1, abs(leftSpeed));
+    analogWrite(LEFT_MOTOR_2, 0);
+    analogWrite(RIGHT_MOTOR_1, abs(rightSpeed));
+    analogWrite(RIGHT_MOTOR_2, 0); 
 }
