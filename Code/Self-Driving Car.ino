5a8b4be27cc9597057672d32e05f1e1159d4ef31
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-11 22:49:22 +0200

    Update Self-Driving Car.ino

--- Code/Self-Driving Car.ino
+++ Code/Self-Driving Car.ino
@@ -69,5 +69,15 @@ void loop() {
     bool onCenter = centerSensorValue > THRESHOLD;
     bool onRight = rightSensorValue > THRESHOLD;
     
+    // Check for obstacles
+    long duration, distance;
+    digitalWrite(TRIGGER_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN, LOW);
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration / 2) / 29.1;
+    
     
 }
