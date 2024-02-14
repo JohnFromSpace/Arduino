79d7fa1c0efb2da7e75776d5c9296571e43e38fe
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-15 00:52:47 +0200

    Update Radio Device.ino

--- Code/Radio Device.ino
+++ Code/Radio Device.ino
@@ -1,1 +1,8 @@
-
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <AESLib.h> // Library for AES encryption
+#include <Wire.h>
+#include <IRremote.h>
+#include <SparkFunBME280.h>
+#include <Adafruit_Sensor.h>
