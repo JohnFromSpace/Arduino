998486bf1b708441a03a4baa2ebac95efbdb93b3
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2024-02-22 23:57:50 +0200

    Update Coffee Machine.ino

--- Code/Coffee Machine.ino
+++ Code/Coffee Machine.ino
@@ -1,1 +1,14 @@
 #include <LiquidCrystal.h>
+
+// Pin definitions
+const int espressoButtonPin = 2;
+const int latteButtonPin = 3;
+const int cappuccinoButtonPin = 4;
+const int pumpPin = 5;
+const int waterLevelPin = A0;
+const int waterTempPin = A1;
+const int waterFilterPin = A2;
+const int grinderPin = 6;
+const int buzzerPin = 7;
+const int strengthButtonPin = 8;
+const int timerButtonPin = 9;
